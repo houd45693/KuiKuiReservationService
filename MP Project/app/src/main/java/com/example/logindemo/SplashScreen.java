bb6e7a20196a43213661f45d40a56a40610e4000
nursyahiraamira <60992106+nursyahiraamira@users.noreply.github.com>
2020-03-22 02:50:52 +0800

    FIXED SPLASH SCREEN ----> USE SIMPLE CODE TO MAKE IT FUNCTION..

--- MP Project/app/src/main/java/com/example/logindemo/SplashScreen.java
+++ MP Project/app/src/main/java/com/example/logindemo/SplashScreen.java
@@ -4,38 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private int SLEEP_TIMER = 4;
+//    private int SLEEP_TIMER = 4;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_splash_screen);
-        getSupportActionBar().hide();
-        LogoLauncher logoLauncher = new LogoLauncher();
-        logoLauncher.start();
-    }
 
-    private class LogoLauncher extends Thread{
-        public void run(){
-            try{
-                sleep(1000 * SLEEP_TIMER);
-            }catch(InterruptedException e){
-                e.printStackTrace();
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
+        }, 3000);
 
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-            startActivity(intent);
-            SplashScreen.this.finish();
-        }
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        setContentView(R.layout.activity_splash_screen);
+//        getSupportActionBar().hide();
+//        LogoLauncher logoLauncher = new LogoLauncher();
+//        logoLauncher.start();
     }
+
+//    private class LogoLauncher extends Thread{
+//        public void run(){
+//            try{
+//                sleep(1000 * SLEEP_TIMER);
+//            }catch(InterruptedException e){
+//                e.printStackTrace();
+//            }
+//
+//            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+//            startActivity(intent);
+//            SplashScreen.this.finish();
+//        }
+//    }
 }
